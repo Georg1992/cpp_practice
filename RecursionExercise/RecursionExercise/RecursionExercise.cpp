4a50961d380b48ccbe10b6ca769eca87cf251f15
Georg Vassilev <selftitled1@gmail.com>
2025-01-25 19:18:24 +0200

    Exercise 2

--- RecursionExercise/RecursionExercise/RecursionExercise.cpp
+++ RecursionExercise/RecursionExercise/RecursionExercise.cpp
@@ -8,19 +8,29 @@ int function_activation_count{ 0 };
 
 double a_penny_doubled_everyday(int, double amount = 0.01);
 
+string recursive_string(string);
+
+int main()
+{
+    double total_amount = a_penny_doubled_everyday(25);
+	string s = recursive_string("Supastat");
+
+    cout << "If I start with a penny and doubled it every day for 25 days, I will have $" << setprecision(10) << total_amount << endl;
+	cout << "String Reversed recursively: " << s << endl;
+
+}
+
 
 double a_penny_doubled_everyday(int n, double amount) {
-    function_activation_count++;
     if (n <= 1)
         return amount;
     return a_penny_doubled_everyday(--n, amount * 2);
 
 }
 
+string recursive_string(string s) {
+	if (s.length() == 1)
+		return s;
+	return recursive_string(s.substr(1, s.length())) + s.at(0);
 
-int main()
-{
-    double total_amount = a_penny_doubled_everyday(25);
-
-    cout << "If I start with a penny and doubled it every day for 25 days, I will have $" << setprecision(10) << total_amount;
 }
